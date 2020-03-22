c60274d8c5bd9b625c18109fb2b54be6c29f0c11
Luke <lmschless@gmail.com>
2020-03-22 11:14:13 -0700

    api is appending text to div. need to figure out how to get media to work.

--- src/weather-ui.js
+++ src/weather-ui.js
@@ -8,14 +8,16 @@ $(document).ready(function() {
 		(async () => {
 			let popularArticles = new PopularArticles();
 			const response = await popularArticles.getArticles();
-			console.log(response);
+			console.log(response.results[0].url);
 			getElements(response);
 		})();
 
 		const getElements = function(response) {
 			if (response) {
-				$('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-				$('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+				$('.showHumidity').append(
+					`<div><h1>${response.results[0].title}</h1><h4>${response.results[0].byline}<p>${response.results[0]
+						.url}</p> <p><img src="${response.results[0].media.media-metadata.url}" alt="" srcset=""></p></div>`
+				);
 			} else {
 				$('.showHumidity').text(`There was an error handling your request.`);
 				$('.showTemp').text(`Please check your inputs and try again!`);
