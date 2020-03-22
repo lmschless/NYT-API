ce1a1f5f4d1d6eaf03872be501283fa4611311fa
Luke <lmschless@gmail.com>
2020-03-22 14:18:22 -0700

    finally got the loop set up correctly to iterate through the results and display each article in the list. working on getting thumbnail images to show next.

--- src/weather-ui.js
+++ src/weather-ui.js
@@ -1,16 +1,30 @@
 import { PopularArticles } from './weather-service.js';
 
 $(document).ready(function() {
+	let test;
 	$('#weatherLocation').click(function() {
-		const city = $('#location').val();
-		$('#location').val('');
+		// const city = $('#location').val();
+		// $('#location').val('');
 
 		(async () => {
 			let popularArticles = new PopularArticles();
 			const response = await popularArticles.getArticles();
-			console.log(response.results[0].url);
-			getElements(response);
+			test = response.results;
+			console.log(response.results);
+			displayResults(response);
+
+			// getElements(response);
 		})();
+
+		const displayResults = (response) => {
+			for (let i = 0; i < response.results.length; i++) {
+				$('.showHumidity').append(
+					`<div><h1>#${i} ${response.results[i].title}</h1><h4>${response.results[i].byline}<p>${response.results[i]
+						.url}</p> <p><img src="https://static01.nyt.com/images/2020/03/21/world/It21italy-virus1sub-copy/21italy-virus1sub-thumbStandard-v2.jpg" alt="" srcset=""></p></div>`
+				);
+				console.log(response);
+			}
+		};
 
 		const getElements = function(response) {
 			if (response) {
