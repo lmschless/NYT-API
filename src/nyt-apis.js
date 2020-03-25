bdde412cdb8cc8fc5d7af8a0656e7ecae589cd71
Luke <lmschless@gmail.com>
2020-03-24 19:47:55 -0700

    working on 2nd api

---
+++ src/nyt-apis.js
@@ -0,0 +1,60 @@
+import './nyt-ui.js';
+import 'bootstrap';
+import './app.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // async/await method for api call
+export class PopularArticles {
+	async getArticles() {
+		try {
+			let response = await fetch(
+				`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=3KfCi2cnPyWhA5FCqS3qlqUKULBAPRGk`
+			);
+			let jsonifiedResponse;
+			if (response.ok && response.status == 200) {
+				jsonifiedResponse = await response.json();
+			} else {
+				jsonifiedResponse = false;
+			}
+			return jsonifiedResponse;
+		} catch (error) {
+			return false;
+		}
+	}
+}
+
+export class SearchArticles {
+	async findArticles(searchTerm) {
+		try {
+			let response = await fetch(
+				`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerm}&api-key=3KfCi2cnPyWhA5FCqS3qlqUKULBAPRGk`
+			);
+			let jsonifiedResponse;
+			if (response.ok && response.status == 200) {
+				jsonifiedResponse = await response.json();
+			} else {
+				jsonifiedResponse = false;
+			}
+			return jsonifiedResponse;
+		} catch (error) {
+			return false;
+		}
+	}
+}
+
+// Fetch method for API calling
+
+// fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
+// 	.then(function(response) {
+// 		if (response.ok && response.status == 200) {
+// 			return response.json();
+// 		} else {
+// 			return false;
+// 		}
+// 	})
+// 	.catch(function(error) {
+// 		return false;
+// 	})
+// 	.then(function(jsonifiedResponse) {
+// 		getElements(jsonifiedResponse);
+
+// 		});
+// });
---
+++ src/nyt-ui.js
@@ -0,0 +1,56 @@
+import { PopularArticles } from './nyt-apis.js';
+import { SearchArticles } from './nyt-apis.js';
+import $ from 'jquery';
+
+$(document).ready(function() {
+	let displayResults;
+	$('#popular-articles').click(function() {
+		(async () => {
+			let popularArticles = new PopularArticles();
+			const response = await popularArticles.getArticles();
+			console.log(response.results);
+			displayResults(response);
+		})();
+	});
+	displayResults = (response) => {
+		if (response.results) {
+			for (let i = 0; i < response.results.length; i++) {
+				$('.display-articles').append(`<div class="media">`);
+				if (response.results[i].media.length != 0) {
+					$('.display-articles').append(
+						`<div class="media-body">
+					<h5 class="mt-0">#${i} ${response.results[i].title}</h5>
+					${response.results[i].byline}<br><img src="${response.results[i].media[0]['media-metadata'][2].url}" class="mr-3"
+					 alt="" srcset="">
+				 </div>
+			 </div>`
+					);
+				} else {
+					$('.display-articles').append(
+						`<br><div class="media-body">
+						<h5 class="mt-0">#${i} ${response.results[i].title}</h5><br>
+						${response.results[i].byline}
+					</div>
+				</div>`
+					);
+				}
+			}
+			console.log(response);
+		} else {
+			for (let i = 0; i < 10; i++) {
+				$('.display-articles').append(`${response.response.docs.headline}`);
+			}
+			console.log(response);
+			console.log('using search, not popular articles!');
+		}
+	};
+
+	$('#search-articles').click(function() {
+		(async () => {
+			let searchArticles = new SearchArticles();
+			const response = await searchArticles.findArticles('virus');
+			console.log(response);
+			displayResults(response);
+		})();
+	});
+});
